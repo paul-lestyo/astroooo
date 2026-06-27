406b4c4cf1f49b5fa798db2bb9afcba5f0179b42
paul-lestyo <lestyo24@gmail.com>
2026-06-27 17:21:57 +0700

    backup before astro upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lestyo.com', // Ubah ke domain Anda saat production
+  site: 'https://paulus-lestyo.my.id',
   output: 'server',
   adapter: node({ mode: 'standalone' }),
   integrations: [tailwind(), react(), keystatic()],
